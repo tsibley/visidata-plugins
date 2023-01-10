3beeb96475e03ccd4995e8065393a438f62fd511
Thomas Sibley <tsibley@fredhutch.org>
2023-01-10 14:36:20 -0800

    git subrepo commit (merge) vendor/ajkerrigan
    subrepo:
      subdir:   "vendor/ajkerrigan"
      merged:   "a7eef8f"
    upstream:
      origin:   "https://github.com/ajkerrigan/visidata-plugins"
      branch:   "main"
      commit:   "b0ad8c0"
    git-subrepo:
      version:  "0.4.1"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "a04d8c2"

--- vendor/ajkerrigan/plugins/vd_jmespath.py
+++ vendor/ajkerrigan/plugins/vd_jmespath.py
@@ -5,41 +5,59 @@ from visidata import BaseSheet, ExprColumn, vd
 
 @BaseSheet.api
 def addcol_jmespath(sheet):
-    expr = sheet.inputExpr("new column jmespath expression=")
-    sheet.addColumnAtCursor(
-        ExprColumn(
-            expr,
-            expr=f"jmespath.search('{expr}', row)",
-            curcol=sheet.cursorCol
-        )
+    try:
+        completer = vd.CompleteExpr
+    except AttributeError:
+        vd.debug("falling back to global for CompleteExpr")
+        from visidata import CompleteExpr
+
+        completer = CompleteExpr
+    expr = vd.input(
+        "new column jmespath expression=",
+        "jmespath-expr",
+        completer=completer(sheet),
     )
+    # Create a partial function based on the provided jmespath expression,
+    # and add it as a sheet attribute. This is one way to avoid edge cases
+    # when evaluating jmespath expressions that contain nested quotes.
+    partial_attr = f"_jmespath_search_{sha1(expr.encode('utf8')).hexdigest()}"
+    setattr(sheet, partial_attr, partial(jmespath.search, expr))
+    sheet.addColumnAtCursor(
+        ExprColumn(expr, expr=f"sheet.{partial_attr}(row)", curcol=sheet.cursorCol)
+    )
+
 
 @BaseSheet.api
 def select_by_jmespath(sheet, unselect=False):
-    action = 'unselect' if unselect else 'select'
-    expr = sheet.inputExpr(f"{action} by jmespath expression=")
+    action = "unselect" if unselect else "select"
+    expr = vd.input(
+        f"{action} by jmespath expression=",
+        "jmespath-expr",
+        completer=vd.CompleteExpr(sheet),
+    )
 
     match_func = partial(jmespath.search, expr)
     select_func = getattr(sheet, action)
     select_func(sheet.gatherBy(match_func), progress=False)
 
+
 BaseSheet.addCommand(
-    '',
-    'addcol-jmespath',
-    'sheet.addcol_jmespath()',
-    'create new column from a jmespath expression'
+    "",
+    "addcol-jmespath",
+    "sheet.addcol_jmespath()",
+    "create new column from a jmespath expression",
 )
 BaseSheet.addCommand(
-    '',
-    'select-jmespath',
-    'sheet.select_by_jmespath()',
-    'select rows matching a jmespath expression in any visible column'
+    "",
+    "select-jmespath",
+    "sheet.select_by_jmespath()",
+    "select rows matching a jmespath expression in any visible column",
 )
 BaseSheet.addCommand(
-    '',
-    'unselect-jmespath',
-    'sheet.select_by_jmespath(unselect=True)',
-    'unselect rows matching a jmespath expression in any visible column'
+    "",
+    "unselect-jmespath",
+    "sheet.select_by_jmespath(unselect=True)",
+    "unselect rows matching a jmespath expression in any visible column",
 )
 
 vd.addGlobals(globals())
--- vendor/ajkerrigan/plugins/vdrepl.py
+++ vendor/ajkerrigan/plugins/vdrepl.py
@@ -1,27 +1,23 @@
-'''
+"""
 Launch an embedded ptipython REPL from within VisiData.
-'''
-
-import sys
+"""
 
 from pathlib import Path
 
-from ptpython.ipython import embed, InteractiveShellEmbed
-from prompt_toolkit.history import ThreadedHistory, FileHistory
+from ptpython.ipython import InteractiveShellEmbed, embed
 from visidata import LazyChainMap, Sheet, SuspendCurses, VisiData, vd
 
 
 class Dummy:
-    '''Hacks to patch sheet-local variables that we want the REPL to ignore.'''
+    """Hacks to patch sheet-local variables that we want the REPL to ignore."""
 
-    replayStatus = 'dummy'
-    someSelectedRows = 'dummy'
+    replayStatus = "dummy"
+    someSelectedRows = "dummy"
 
 
 @VisiData.api
 def openRepl(self):
-    orig_stdin = sys.stdin
-    orig_stdout = sys.stdout
+    """Open a ptipython-based REPL that inherits VisiData's context."""
     try:
         # Provide local top-level access to VisiData global and sheet-local
         # variables, similar to VisiData's `execCommand` context.
@@ -32,25 +28,29 @@ def openRepl(self):
 
     with SuspendCurses():
         try:
-            sys.stdin = vd._stdin
-            sys.stdout = open('/dev/tty', mode='w')
             history_file = (
                 Path(vd.options.visidata_dir).expanduser()
-                / 'cache'
-                / 'ptpython'
-                / 'history'
+                / "cache"
+                / "ptpython"
+                / "history"
             )
             Path.mkdir(history_file.parent, parents=True, exist_ok=True)
             shell = InteractiveShellEmbed.instance(
-                history_filename=str(history_file), vi_mode=True,
+                history_filename=str(history_file),
+                vi_mode=True,
             )
-            shell.python_input.title = 'VisiData IPython REPL (ptipython)'
+            shell.python_input.title = "VisiData IPython REPL (ptipython)"
             shell.python_input.show_exit_confirmation = False
             embed()
         except Exception as e:
             vd.exceptionCaught(e)
         finally:
-            sys.stdin = orig_stdin
-            sys.stdout = orig_stdout
+            # The embedded IPython session is a singleton by default,
+            # but launching it via `open-repl` in VisiData a second time
+            # seems to either freeze or leave an exit message up from the
+            # previous instance. Clean out the existing instance so any
+            # future invocations get a fresh start.
+            InteractiveShellEmbed.clear_instance()
 
-Sheet.addCommand("`", "open-repl", "vd.openRepl()")
+
+Sheet.addCommand("", "open-repl", "vd.openRepl()")
